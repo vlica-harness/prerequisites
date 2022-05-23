4ac7202d28cf52d917b135eac73b6e2e5d047a70
Vasile Lica <vasile.lica@harness.io>
2022-05-23 13:18:55 +0300

    adding terraform files

---
+++ terraform/localresource/config.tf
@@ -0,0 +1,10 @@
+variable "countt" {
+  type = number
+}
+
+resource "null_resource" "example" {
+  count = "${var.countt}"
+  provisioner "local-exec" {
+    command = "echo testlocallocaltesttestlocallocaltest"
+  }
+}
