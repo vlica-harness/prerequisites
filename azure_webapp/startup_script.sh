724230211b97369b6d227117b5438ee4e7c25b8f
Vasile Lica <vasile.lica@harness.io>
2022-07-20 15:06:14 +0300

    no message

--- azure_webapp/startup_script.sh
+++ azure_webapp/startup_script.sh
@@ -1,1 +1,1 @@
-/bin/echo echo "httpd started" && trap "exit 0;" TERM INT; httpd -v -p $PORT -h /www -f & wait
+/bin/echo "httpd started" && trap "exit 0;" TERM INT; httpd -v -p $PORT -h /www -f & wait
