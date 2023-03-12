73df312e67be2c90ef0a3d5faabd2208ba0fa5a0
Vasile Lica <vasile.lica@harness.io>
2023-03-12 15:09:48 +0200

    add prereq backe

--- terraform/localresource/config.tf
+++ terraform/localresource/config.tf
@@ -2,8 +2,18 @@ variable "countt" {
   type = number
 }
 
+terraform {
+  backend "azurerm" {
+    key                  = "vlica-test/terraform.tfstate"
+    subscription_id      = "12d2db62-5aa9-471d-84bb-faa489b3e319"
+    resource_group_name  = "tfResourceGroup"
+    storage_account_name = "vlicaterraformremoteback"
+    container_name       = "vl-azure-test"
+  }
+}
+
 resource "null_resource" "example" {
-  count = "${var.countt}"
+  count = var.countt
   provisioner "local-exec" {
     command = "echo testlocallocaltesttestlocallocaltest"
   }
