fda782caa160b4db1334fbb04508487805dae091
Vasile Lica <vasile.lica@harness.io>
2023-03-12 15:28:42 +0200

    add

--- terraform/azure-resource-group-local/main.tf
+++ terraform/azure-resource-group-local/main.tf
@@ -11,3 +11,18 @@ resource "azurerm_resource_group" "az-rg" {
 
   tags = local.all_tags
 }
+
+
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    key                  = "vlica-test/terraform.tfstate"
+    subscription_id      = "12d2db62-5aa9-471d-84bb-faa489b3e319"
+    resource_group_name  = "tfResourceGroup"
+    storage_account_name = "vlicaterraformremoteback"
+    container_name       = "vl-azure-test"
+  }
+}
