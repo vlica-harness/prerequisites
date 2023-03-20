8e679b51b2c527b6e5753ca99e4f6ce7e4eccad5
Vasile Lica <vasile.lica@harness.io>
2023-03-20 17:21:44 +0200

    add .

--- terraform/localresource/config.tf
+++ terraform/localresource/config.tf
@@ -2,16 +2,6 @@ variable "countt" {
   type = number
 }
 
-terraform {
-  backend "azurerm" {
-    key                  = "vlica-test/terraform.tfstate"
-    subscription_id      = "12d2db62-5aa9-471d-84bb-faa489b3e319"
-    resource_group_name  = "tfResourceGroup"
-    storage_account_name = "vlicaterraformremoteback"
-    container_name       = "vl-azure-test"
-  }
-}
-
 resource "null_resource" "example" {
   count = var.countt
   provisioner "local-exec" {
