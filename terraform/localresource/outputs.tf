2edffd4598f308b311fa6514d358fda8ad0957d2
Vasile Lica <vasile.lica@harness.io>
2023-06-16 10:31:55 +0300

    correct values

--- terraform/localresource/outputs.tf
+++ terraform/localresource/outputs.tf
@@ -1,7 +1,7 @@
 output "test-output-name1" {
-  value = "test-output-name1"
+  value = "test-output-value1"
 }
 
 output "test-output-name2" {
-  value = "test-output-name2"
+  value = "test-output-value2"
 }
