0c195babf5d66685e4bdd69c56c29f95c0641340
Vasile Lica <vasile.lica@harness.io>
2024-03-11 19:43:07 +0200

    add another output

--- terraform/localresource/outputs.tf
+++ terraform/localresource/outputs.tf
@@ -5,3 +5,17 @@ output "TEST_OUTPUT_NAME1" {
 output "test-output-name2" {
   value = "test-output-value2"
 }
+
+output "result" {
+  value = {
+    "region" : "us-east-1",
+    "hostInstances" : [
+      {
+        "hostname" : "hostnamexxxx",
+        "defaultIpAddress" : "defaultyyyyy"
+      }
+    ],
+    "hostname" : "hostnamexxxx",
+    "defaultIpAddress" : "defaultyyyyy"
+  }
+}
