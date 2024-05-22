8a178561bb82c13c345327d3f0ecd6da93200bb6
Vasile Lica <vasile.lica@harness.io>
2024-05-22 10:22:47 +0300

    add terraform cloud outputs

--- terraform-cloud/outputs.tf
+++ terraform-cloud/outputs.tf
@@ -5,3 +5,10 @@ output "test-output-name1" {
 output "test-output-name2" {
   value = "test-output-name2"
 }
+
+output "clusters_discovered" {
+    value = {
+        "test-use1-blue",
+        "test-use1-green",
+        }
+}
