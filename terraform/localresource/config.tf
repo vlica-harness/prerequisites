e55b82f6232480d27eef1b9a3b36d71990ca98e7
Vasile Lica <vasile.lica@harness.io>
2024-11-25 18:46:29 +0200

    add comma

--- terraform/localresource/config.tf
+++ terraform/localresource/config.tf
@@ -1,5 +1,4 @@
 variable "swag_file" {
-  default = "config.tmpl" # Path to the template file
 }
 
 
@@ -7,5 +6,5 @@ resource "local_file" "rendered_config" {
   filename = "output_config.conf" # Output file
 
   # Use the templatefile function to populate the content
-  content = templatefile(var.swag_file {})
+  content = templatefile(var.swag_file, {})
 }
