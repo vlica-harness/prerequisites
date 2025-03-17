9e477c62bba5597033591173de76ab32b62e7079
Vasile Lica <vasile.lica@harness.io>
2025-03-17 12:09:38 +0200

    switch worksapce

--- terraform-cloud/main.tf
+++ terraform-cloud/main.tf
@@ -12,7 +12,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "harness-dev"
     workspaces {
-      name = "vl-prerequisites-cloud-run-1"
+      name = "vl-workspace-prereq-test-1"
     }
   }
 }
