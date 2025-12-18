b8ecec9223101cd0d2e494ad280eb576b5bca44f
Vasile Lica <vasile.lica@harness.io>
2025-12-18 16:41:50 +0200

    add secret aws

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,10 +1,24 @@
-resource "aws_instance" "my_aws_instance_vl" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  
+#resource "aws_instance" "my_aws_instance_vl" {
+#  ami           = var.ami_id
+#  instance_type = var.instance_type
+#  
+#  tags = {
+#    Name = var.name_tag
+#  }
+#}
+
+
+resource "aws_secretsmanager_secret" "vl-entity1-secret" {
+  name = "vl-entity1-secret"
+
   tags = {
-    Name = var.name_tag
+    name = var.name_tag
   }
+}
+
+resource "aws_secretsmanager_secret_version" "vl-entity1-secret" {
+  secret_id     = aws_secretsmanager_secret.vl-entity1-secret.id
+  secret_string = "my-plain-text-secret" 
 }
 
 provider "aws" {}
